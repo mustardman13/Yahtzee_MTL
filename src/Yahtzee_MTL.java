bcb0a2ad961adeba7ac677d41d3e62d6d883452c
mustardman13 <87210095+mustardman13@users.noreply.github.com>
2021-07-10 19:28:57 -0600

    Update Yahtzee_MTL.java

--- src/Yahtzee_MTL.java
+++ src/Yahtzee_MTL.java
@@ -17,7 +17,7 @@ import acm.program.GraphicsProgram;
 /**
  * Practice application for learning how to code in Java and use git hub
  * Let's up to 6 players play yahtzee together ... on the same computer
- * xxx
+ * will gradually add more options.
  * @author mustardman13
  *
  */
